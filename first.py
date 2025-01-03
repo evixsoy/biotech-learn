40213c281771f63f848f566588ef84986fdef3d7
evixsoy <ondra.kosatik1@gmail.com>
2025-01-03 23:10:34 +0100

    cviceni 1

--- first.py
+++ first.py
@@ -1,1 +1,32 @@
-print(test)
+# 1. Počítání nukleotidů v DNA sekvenci
+# Popis:
+# Vytvoř program, který:
+
+# Načte DNA sekvenci ze souboru nebo zadání uživatelem.
+# Spočítá, kolik je jednotlivých nukleotidů (A, T, G, C).
+# Určí podíl jednotlivých nukleotidů.
+
+sequence = "ATGCTTAGC"
+count_a = 0
+count_t = 0
+count_g = 0
+count_c = 0
+
+for i in sequence:
+    if i == "A":
+        count_a +=1
+    if i == "T":
+        count_t +=1
+    if i == "G":
+        count_g +=1
+    if i == "C":
+        count_c +=1
+print(f"Pocet A :{count_a}\nPocet T :{count_t}\nPocet G :{count_g}\nPocet C :{count_c}\n ")
+
+# 2. Reverzní komplement DNA
+# Popis:
+# Vytvoř nástroj, který:
+
+# Načte DNA sekvenci.
+# Vrátí její reverzní komplement (vymění A ↔ T a G ↔ C, a zároveň otočí sekvenci).
+
