5637b960722e99d30c18b4af97b464e35cc7cb64
evixsoy <ondra.kosatik1@gmail.com>
2025-01-04 20:22:27 +0100

    cviceni 3

--- first.py
+++ first.py
@@ -32,22 +32,67 @@
 # Načte DNA sekvenci.
 # Vrátí její reverzní komplement (vymění A ↔ T a G ↔ C, a zároveň otočí sekvenci).
 
-sequence = "AAGCTTGACCTG"
-changedseq  = ""
-reversechseq  = ""
+# sequence = "AAGCTTGACCTG"
+# changedseq  = ""
+# reversechseq  = ""
 
-for i in sequence:
-    if i == "A":
-        i = "T"
-    elif i == "T": 
-        i = "A"
-    elif i == "G":
-        i = "C"
-    elif i == "C": 
-        i = "G"
-    changedseq = changedseq + i
+# for i in sequence:
+#     if i == "A":
+#         i = "T"
+#     elif i == "T": 
+#         i = "A"
+#     elif i == "G":
+#         i = "C"
+#     elif i == "C": 
+#         i = "G"
+#     changedseq = changedseq + i
 
-print(changedseq)
-print(changedseq[::-1])
+# print(changedseq)
+# print(changedseq[::-1])
 
-    
+
+
+
+
+# Projekt 3: Hledání podsekvencí v DNA
+# Cíl projektu: Vytvořit program, který:
+
+# Načte DNA sekvenci od uživatele nebo ze souboru.
+# Hledá, kolikrát se v sekvenci vyskytuje určitá podsekvence (například ATG).
+# Zobrazí všechny pozice, na kterých se podsekvence nachází (volitelně).
+
+inputvalue = "ATGCGATGCGTACGATG"
+searchvalue = "GCG"
+inputvaluelist = []
+searchvaluelist = []
+found = 0
+foundloc = ""
+
+for i in inputvalue:
+    if i =="A" or i=="T" or i =="G" or i=="C":
+        inputvaluelist.append(i)
+    else:
+        print("Neplatne znaky")
+
+for i in searchvalue:
+    if i =="A" or i=="T" or i =="G" or i=="C":
+        searchvaluelist.append(i)
+    else:
+        print("Neplatne znaky")
+
+searchvaluelen = len(searchvalue)
+templist = []
+
+for i in range(0, len(inputvalue)-2):
+    for g in range(0,searchvaluelen):
+        templist.append(inputvalue[i+g])
+
+    if templist == searchvaluelist:
+        print("nalezeno")
+        print(i)
+        found +=1
+        foundloc = foundloc + f"{i+1}|" 
+    templist.clear()
+
+print(f"pocet vyskytu:{found}")
+print(f"nalezeno na :{foundloc}")
