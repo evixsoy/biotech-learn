6f8157e17c79280bbdf876af0420c3e9d6c14046
evixsoy <ondra.kosatik1@gmail.com>
2025-01-05 22:35:15 +0100

    cviceni 5 progress

--- first.py
+++ first.py
@@ -109,70 +109,153 @@
 # Délku sekvence.
 # (Volitelné) Distribuci velikosti genů (pokud FASTA obsahuje více sekvencí).
 
-filepath = "human dna.fa"
-delkaseq = 25
+# filepath = "human dna.fa"
+# delkaseq = 25
 
-with open(filepath, 'r') as openfile:
-    fileitems = openfile.read()
-#pro jednotlive delkaseq
+# with open(filepath, 'r') as openfile:
+#     fileitems = openfile.read()
+# #pro jednotlive delkaseq
 
-listitems = []
-for i in fileitems:
-    if i =="\n":
-        pass
-    else:
-        listitems.append(i)
+# listitems = []
+# for i in fileitems:
+#     if i =="\n":
+#         pass
+#     else:
+#         listitems.append(i)
 
-dictseq= {}
-pocet = len(fileitems)//delkaseq
-print(pocet)
-for g in range(pocet):
-    templist =[]
-    for i in range(delkaseq):
-        index = g * delkaseq + i 
-        if index >= len(listitems):  
-            break
-        templist.append(listitems[index])
-        if i == delkaseq-1:
-            dictseq[g] = templist
-    if g >= pocet-1:
-        break
-    else:
-        count_a = 0
-        count_t = 0
-        count_g = 0
-        count_c = 0
-        if index >= len(listitems): 
-            break
-        for i in dictseq[g]:
-             if i == "A":
-                count_a +=1
-             elif i == "T":
-                 count_t +=1
-             elif i == "G":
-                 count_g +=1
-             elif i == "C":
-                count_c +=1
-        print(f"{g+1}. Sekvence | Pocet A :{count_a} | Pocet T :{count_t} | Pocet G :{count_g} | Pocet C :{count_c}| GC obsah: {((count_g + count_c)/delkaseq)*100:.1f}%")
+# dictseq= {}
+# pocet = len(fileitems)//delkaseq
+# print(pocet)
+# for g in range(pocet):
+#     templist =[]
+#     for i in range(delkaseq):
+#         index = g * delkaseq + i 
+#         if index >= len(listitems):  
+#             break
+#         templist.append(listitems[index])
+#         if i == delkaseq-1:
+#             dictseq[g] = templist
+#     if g >= pocet-1:
+#         break
+#     else:
+#         count_a = 0
+#         count_t = 0
+#         count_g = 0
+#         count_c = 0
+#         if index >= len(listitems): 
+#             break
+#         for i in dictseq[g]:
+#              if i == "A":
+#                 count_a +=1
+#              elif i == "T":
+#                  count_t +=1
+#              elif i == "G":
+#                  count_g +=1
+#              elif i == "C":
+#                 count_c +=1
+#         print(f"{g+1}. Sekvence | Pocet A :{count_a} | Pocet T :{count_t} | Pocet G :{count_g} | Pocet C :{count_c}| GC obsah: {((count_g + count_c)/delkaseq)*100:.1f}%")
 
 #pro celou sekvenci
-count_a = 0
-count_t = 0
-count_g = 0
-count_c = 0
+# count_a = 0
+# count_t = 0
+# count_g = 0
+# count_c = 0
 
-for i in fileitems:
-    if i == "A":
-        count_a +=1
-    elif i == "T":
-        count_t +=1
-    elif i == "G":
-        count_g +=1
-    elif i == "C":
-        count_c +=1
-print(f"Pocet A :{count_a}\nPocet T :{count_t}\nPocet G :{count_g}\nPocet C :{count_c}")
-print(f"delka: {len(fileitems)}")
-print(f"GC obsah: {((count_g + count_c)/len(fileitems))*100:.1f}%")
+# for i in fileitems:
+#     if i == "A":
+#         count_a +=1
+#     elif i == "T":
+#         count_t +=1
+#     elif i == "G":
+#         count_g +=1
+#     elif i == "C":
+#         count_c +=1
+# print(f"Pocet A :{count_a}\nPocet T :{count_t}\nPocet G :{count_g}\nPocet C :{count_c}")
+# print(f"delka: {len(fileitems)}")
+# print(f"GC obsah: {((count_g + count_c)/len(fileitems))*100:.1f}%")
 
+
+
+
+
+
+
+
+
+
+# 5. Překlad DNA do proteinů
+# Popis:
+# Program by měl:
+# Převést DNA sekvenci na RNA (A → U, T → A, G ↔ C).
+# Přeložit RNA sekvenci do aminokyselin (pomocí genetického kódu).
+# Vypisovat odpovídající proteiny.
+# Klíčové dovednosti:
+# Použití slovníků pro genetický kód.
+# Zpracování dat v blocích (tripletech).
+
+
+filepath = "human dna.fa"
+
+with open(filepath, 'r') as openfile:
+    fileitemsdna = openfile.read()
+
+#preklad do rna
+rnaitems = ""
+for i in fileitemsdna:
+    if i == "T":
+        rnaitems += "U"  
+    elif i != "\n":  
+        rnaitems += i
+#kodovani do proteinu
+
+# Tabulka genetického kódu
+genetic_code = {
+    "ATA":"I", "ATC":"I", "ATT":"I",
+    "ACA":"T", "ACC":"T", "ACG":"T", "ACT":"T",
+    "AAC":"N", "AAT":"N", "AAA":"K", "AAG":"K",
+    "AGC":"S", "AGT":"S", "AGA":"R", "AGG":"R",
+    "CTA":"L", "CTC":"L", "CTG":"L", "CTT":"L",
+    "CCA":"P", "CCC":"P", "CCG":"P", "CCT":"P",
+    "CAC":"H", "CAT":"H", "CAA":"Q", "CAG":"Q",
+    "CGA":"R", "CGC":"R", "CGG":"R", "CGT":"R",
+    "GTA":"V", "GTC":"V", "GTG":"V", "GTT":"V",
+    "GCA":"A", "GCC":"A", "GCG":"A", "GCT":"A",
+    "GAC":"D", "GAT":"D", "GAA":"E", "GAG":"E",
+    "GGA":"G", "GGC":"G", "GGG":"G", "GGT":"G",
+    "TCA":"S", "TCC":"S", "TCG":"S", "TCT":"S",
+    "TTC":"F", "TTT":"F", "TTA":"L", "TTG":"L",
+    "TAC":"Y", "TAT":"Y"
+}
+start = "AUG"
+end = ["TAA", "TAG", "TGA"]
+rnaitemslen = len(rnaitems)
+
+found = ""
+hledani = 0
+pocitadlo =0
+check = 0
+for i in range(1,rnaitemslen-2):
+    trojice = rnaitems[i] +rnaitems[i+1] +rnaitems[i+2]     
+    if hledani <3:
+        hledani +=1
+    elif hledani ==3:
+        if trojice == "UAA" or trojice =="UAG" or trojice == "UGA" or trojice == start:
+            pass
+        else:
+            found+=trojice
+    # print(rnaitems[i] +rnaitems[i+1] +rnaitems[i+2]
+    if trojice == start and check ==1:
+        hledani = 0
+        check =0
+    if trojice == start:
+        print("found")
+        found+=trojice
+        hledani =1
+        check = 1
+    if trojice == "UAA" or trojice =="UAG" or trojice == "UGA":
+        hledani = 0
+        print(found)
+        found =""
+        check = 0
 
 
