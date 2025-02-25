a12335f0f1dc8cfb4d4798ebe4a5bc4821964eaf
evixsoy <ondra.kosatik1@gmail.com>
2025-02-25 23:08:45 +0100

    rabbits cviceni

---
+++ rosalind-projects/5. Rabbits and Recurrence Relations.py
@@ -0,0 +1,14 @@
+print(f"Zadej počet měsíců:")
+month_input = int(input(">"))
+print(f"Zadej počet rozm. párů:")
+rozmpary_input = int(input(">"))
+
+def vypocet(month,rozmpary):
+    #prvni mesic je pouze jeden mladej kralik
+    d,m =0,1
+    for i in range(month -1):
+        # mlady kralici jsou dospeli krat koeficient rozmnozovani, dospeli jsou dospeli kralici plus mladi kralici z minulyho mesice (vyrostli)
+        m,d = d*rozmpary , d +m
+    return m+d
+
+print(vypocet(month_input, rozmpary_input))
